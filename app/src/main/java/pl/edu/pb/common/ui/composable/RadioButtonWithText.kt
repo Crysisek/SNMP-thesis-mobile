ee12da5df392d68ce480e38f311c5f800a3a48be
Kacper Kalinowski <kacper.kalinowski@intive.com>
2023-01-11 19:26:55 +0100

    Last changes

--- app/src/main/java/pl/edu/pb/common/ui/composable/RadioButtonWithText.kt
+++ app/src/main/java/pl/edu/pb/common/ui/composable/RadioButtonWithText.kt
@@ -1,9 +1,6 @@
 package pl.edu.pb.common.ui.composable
 
-import androidx.annotation.StringRes
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.RadioButton
@@ -11,7 +8,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.unit.dp
 
